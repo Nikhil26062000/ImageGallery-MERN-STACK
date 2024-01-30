9d5f9a0ca3368c404022b7fcac262ae3a9549daf
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-30 18:04:35 +0530

    Project Setup : - Initial commit - Add dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./index.css"
+
+const App = () => {
+  return (
+    <div className="text-center text-violet-600 text-3xl">
+      This is Image Gallery app
+    </div>
+  )
+}
+
+export default App
